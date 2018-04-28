1f0ed06c03c5fe159e3721cea27fa5ffa9b24ed4
Woody-Hu <812791840@qq.com>
2018-04-28 09:51:37 +0800

    2018 04 28

---
+++ ToolCode/SerializeDic.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace ToolCode
+{
+    [Serializable]
+    public class SerializeDic<TKey, TValue> : Dictionary<TKey, TValue>, IXmlSerializable
+    {
+        private const string m_useNodeName = "DicNode";
+
+        private const string m_useKeyNodeName = "KeyNode";
+
+        private const string m_useValueNodeName = "ValueNode";
+
+        /// <summary>
+        /// 普通构造
+        /// </summary>
+        public SerializeDic()
+            : base()
+        {
+
+        }
+
+        /// <summary>
+        /// 键值比较器构造
+        /// </summary>
+        /// <param name="comparer"></param>
+        public SerializeDic(IEqualityComparer<TKey> comparer)
+            : base(comparer)
+        {
+
+        }
+
+
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 读取方法
+        /// </summary>
+        /// <param name="reader"></param>
+        public void ReadXml(XmlReader reader)
+        {
+            XmlSerializer keySerializer = new XmlSerializer(typeof(TKey));
+            XmlSerializer valueSerializer = new XmlSerializer(typeof(TValue));
+            bool wasEmpty = reader.IsEmptyElement;
+            reader.Read();
+            if (wasEmpty) return;
+
+            while (reader.NodeType != XmlNodeType.EndElement)
+            {
+
+                reader.ReadStartElement(m_useNodeName);
+
+                reader.ReadStartElement(m_useKeyNodeName);
+                TKey key = (TKey)keySerializer.Deserialize(reader);
+                reader.ReadEndElement();
+
+                reader.ReadStartElement(m_useValueNodeName);
+                TValue value = (TValue)valueSerializer.Deserialize(reader);
+                reader.ReadEndElement();
+
+                this.Add(key, value);
+                reader.ReadEndElement();
+
+                reader.MoveToContent();
+            }
+
+            reader.ReadEndElement();
+        }
+
+        /// <summary>
+        /// 写出方法
+        /// </summary>
+        /// <param name="writer"></param>
+        public void WriteXml(XmlWriter writer)
+        {
+            XmlSerializer keySerializer = new XmlSerializer(typeof(TKey));
+            XmlSerializer valueSerializer = new XmlSerializer(typeof(TValue));
+            foreach (TKey key in this.Keys)
+            {
+                writer.WriteStartElement(m_useNodeName);
+
+                writer.WriteStartElement(m_useKeyNodeName);
+                keySerializer.Serialize(writer, key);
+                writer.WriteEndElement();
+
+                writer.WriteStartElement(m_useValueNodeName);
+                TValue value = this[key];
+                valueSerializer.Serialize(writer, value);
+                writer.WriteEndElement();
+
+                writer.WriteEndElement();
+            }
+        }
+    }
+}
---
+++ ToolCode/ToleranceManger.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ToolCode
+{
+    /// <summary>
+    /// 容差管理器 单例模式
+    /// </summary>
+    public class ToleranceManger
+    {
+        /// <summary>
+        /// 默认容差
+        /// </summary>
+        private const double m_default = 1e-9;
+
+        /// <summary>
+        /// 使用的缓存字典
+        /// </summary>
+        private SerializeDic<string, double> m_useDic = null;
+
+        /// <summary>
+        /// 使用的默认路径
+        /// </summary>
+        private const string m_usePath = @"c:\tolerance.xml";
+
+        /// <summary>
+        /// 私有构造从文件中加载
+        /// </summary>
+        private ToleranceManger()
+        {
+            LoadFromFile();
+        }
+
+        /// <summary>
+        /// 析构保存到文件
+        /// </summary>
+        ~ToleranceManger()
+        {
+            SaveToFile();
+        }
+
+        /// <summary>
+        /// 使用的单例模式标签
+        /// </summary>
+        private static ToleranceManger m_useSingleTon = null;
+
+        /// <summary>
+        /// 使用的单例模式管理器
+        /// </summary>
+        /// <returns></returns>
+        public static ToleranceManger GetToleranceManger()
+        {
+            if (null == m_useSingleTon)
+            {
+                m_useSingleTon = new ToleranceManger();
+            }
+
+            return m_useSingleTon;
+        }
+
+        /// <summary>
+        /// 获取使用容差
+        /// </summary>
+        /// <param name="inputName">使用的容差名称</param>
+        /// <returns></returns>
+        public double GetTolerance(string inputName = null)
+        {
+            if (string.IsNullOrWhiteSpace(inputName))
+            {
+                return m_default;
+            }
+
+            if (!m_useDic.ContainsKey(inputName))
+            {
+                m_useDic.Add(inputName, m_default);
+            }
+
+            return m_useDic[inputName];
+        }
+
+        /// <summary>
+        /// 调整使用的特征的容差
+        /// </summary>
+        /// <param name="inputName"></param>
+        /// <param name="inputValue"></param>
+        public void SetTolerance(string inputName, double inputValue)
+        {
+            if (string.IsNullOrWhiteSpace(inputName))
+            {
+                return;
+            }
+
+            if (!m_useDic.ContainsKey(inputName))
+            {
+                m_useDic.Add(inputName, inputValue);
+                return;
+            }
+            m_useDic[inputName] = inputValue;
+        }
+
+        /// <summary>
+        /// 从文件中读取
+        /// </summary>
+        private void LoadFromFile()
+        {
+            if (File.Exists(m_usePath))
+            {
+                try
+                {
+                    XmlSerializer useXmlSercializer = new XmlSerializer(typeof(SerializeDic<string, double>));
+                    m_useDic = useXmlSercializer.Deserialize(File.Open(m_usePath, FileMode.Open)) as SerializeDic<string, double>;
+                }
+                catch (Exception)
+                {
+                    m_useDic = null;
+                }
+
+            }
+
+            if (null == m_useDic)
+            {
+                m_useDic = new SerializeDic<string, double>();
+            }
+
+
+        }
+
+        /// <summary>
+        /// 保存到文件
+        /// </summary>
+        private void SaveToFile()
+        {
+            try
+            {
+                if (File.Exists(m_usePath))
+                {
+                    File.Delete(m_usePath);
+                }
+
+                XmlSerializer useXmlSercializer = new XmlSerializer(typeof(SerializeDic<string, double>));
+                useXmlSercializer.Serialize(File.Create(m_usePath), m_useDic);
+            }
+            catch (Exception)
+            {
+                ;
+            }
+        }
+    }
+}
