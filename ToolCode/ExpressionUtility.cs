9ff0f3012a67694d404055faa59be51db3b4a99e
Woody-Hu <812791840@qq.com>
2018-06-22 09:30:41 +0800

    2018 06 22

--- ToolCode/ExpressionUtility.cs
+++ ToolCode/ExpressionUtility.cs
@@ -7,22 +7,35 @@ using System.Threading.Tasks;
 
 namespace ToolCode
 {
+    /// <summary>
+    /// 表达式树工具
+    /// </summary>
     public class ExpressionUtility
     {
+
         /// <summary>
-        /// 利用输入类的属性与输入容器制作( == ||)关系的表达式树
+        /// 利用输入类的属性与输入容器制作表达式树
         /// </summary>
-        /// <typeparam name="TClass"></typeparam>
-        /// <typeparam name="TProperty"></typeparam>
-        /// <param name="inputProperty"></param>
-        /// <param name="inputContainsSet"></param>
-        /// <returns></returns>
-        public static Func<TClass, bool> GetEqulaOrExpression<TClass, TProperty>(Func<TClass, TProperty> inputProperty, HashSet<TProperty> inputContainsSet)
+        /// <typeparam name="TClass">使用的类泛型</typeparam>
+        /// <typeparam name="TProperty">使用的类属性泛型</typeparam>
+        /// <param name="inputProperty">使用的获取属性的Func</param>
+        /// <param name="inputContainsSet">使用的容器</param>
+        /// <param name="inputComparer">使用的比较Func 第一个参数为从输入中取出的值，第二个参数为从容器中取出的值</param>
+        /// <returns>使用的表达式树</returns>
+        public static Func<TClass, bool> GetEqulaOrExpression<TClass, TProperty>
+            (Func<TClass, TProperty> inputProperty, IEnumerable<TProperty> inputContainsSet, Func<TProperty, TProperty, bool> inputComparer = null)
         {
+            if (null == inputComparer)
+            {
+                inputComparer = GetDefaultFunc<TProperty>();
+            }
+
             //获取输入的参数
             ParameterExpression useInputExpression = Expression.Parameter(typeof(TClass));
 
             var tempMethod = inputProperty.Method;
+
+            var tempComparerMethod = inputComparer.Method;
 
             var useValue = Expression.Call(tempMethod,useInputExpression);
 
@@ -38,11 +51,21 @@ namespace ToolCode
             //表达组合
             foreach (var oneExpression in lstUseConstrant)
             {
-                returnExpression = Expression.Or(returnExpression, Expression.Equal(useValue, oneExpression));
+                returnExpression = Expression.Or(returnExpression, Expression.Call(tempComparerMethod, useValue, oneExpression));
             }
 
 
             return Expression.Lambda<Func<TClass, bool>>(returnExpression,useInputExpression).Compile();
+        }
+
+        /// <summary>
+        /// 使用的默认比较器
+        /// </summary>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <returns></returns>
+        private static Func<TProperty, TProperty, bool> GetDefaultFunc<TProperty>()
+        {
+            return (k1, k2) => k1.Equals(k2);
         }
     }
 }
